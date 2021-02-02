aba4f05f90edfee3e1f8abb3eddce187fe4e0aa7
jhomon <greenman7@gaail.com>
2021-02-02 17:54:26 +0900

    celery 기본 세팅

---
+++ reviewzip/tasks.py
@@ -0,0 +1,15 @@
+from celery import shared_task
+from .models import Review, Sentence, Keyword
+
+""" 입력받은 리뷰 csv 파일로부터
+    해당 리뷰들을 문장 단위로 쪼개고 긍정, 부정을 판단
+    키워드 추출하고 문장에 히당하는 키워드 매핑하여
+    키워드, 문장을 데이터베이스(모델) 에 저장
+ """
+@shared_task
+def predict_sentiment(file):
+    pass
+
+@shared_task
+def match_keyword(sentence):
+    pass
