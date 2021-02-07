53045e9adc23e4d40d316eb89ddaf24b68cf565b
jhomon <greenman7@gmail.com>
2021-02-07 11:10:35 +0900

    키워드 가진 문장 fetch에서 pagination 구현

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
+from .views import SentenceViewSet
 
 app_name = 'api'
 
 urlpatterns = [
-    path('get/review/sentences', views.get_sentences_with_keyword, \
+    path('get/review/sentences', SentenceViewSet.as_view({'get': 'list'}), \
         name='get_sentences_with_keyword'),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,30 +1,24 @@
-from rest_framework.decorators import api_view
-from rest_framework.decorators import parser_classes
-from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.pagination import PageNumberPagination
 from reviewzip.models import Sentence, Review
 from .serializers import SentenceSerializer
 
 # Create your views here.
+class SentenceViewSet(ReadOnlyModelViewSet):
+    """ 키워드에 해당하는 리뷰 문장을 보여주기 위한 viewset """
+    serializer_class = SentenceSerializer
+    pagination_class = PageNumberPagination
 
-@api_view(['GET'])
-#@parser_classes([JSONParser])
-def get_sentences_with_keyword(request, format=None):
-    """
-    review에서 keyword가 들어간 문장들을 리턴
-    """
-    keyword = request.GET.get('keyword')
-    review_name = request.GET.get('review_name')
-    positive = request.GET.get('positive')
+    def get_queryset(self):
+        keyword = self.request.GET.get('keyword')
+        review_name = self.request.GET.get('review_name')
+        positive = self.request.GET.get('positive')
 
-    if positive == 'positive':
-        review = Review.objects.prefetch_related('positive_keyword').get(name=review_name)
-        queryset = \
-            review.positive_keyword.prefetch_related('sentence').get(name=keyword).sentence
-    elif positive == 'negative':
-        review = Review.objects.prefetch_related('negative_keyword').get(name=review_name)
-        queryset = \
-            review.negative_keyword.prefetch_related('sentence').get(name=keyword).sentence
+        if positive == 'positive':
+            review = Review.objects.prefetch_related('positive_keyword').get(name=review_name)
+            return review.positive_keyword.prefetch_related('sentence').get(name=keyword).sentence.all()
+        elif positive == 'negative':
+            review = Review.objects.prefetch_related('negative_keyword').get(name=review_name)
+            return review.negative_keyword.prefetch_related('sentence').get(name=keyword).sentence.all()
 
-    serializer = SentenceSerializer(queryset, many=True)
-    return Response(serializer.data)
