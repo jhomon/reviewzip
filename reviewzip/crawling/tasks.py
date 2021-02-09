38e5a0f6328fb3c2de9b90d4e0aa0e1c2a983398
jhomon <greenman7@gmail.com>
2021-02-09 13:45:30 +0900

    쿠팡 크롤링 추가

--- reviewzip/crawling/tasks.py
+++ reviewzip/crawling/tasks.py
@@ -7,6 +7,7 @@ import re
 import lxml
 import csv
 import os
+import datetime, time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -24,6 +25,18 @@ def pend_product_url(product_url):
 
     # 크롤링 함수 큐에 넣기
     crawling_start.delay()
+
+
+# 일정 시간 동안 계속 50px씩 아래로 스크롤
+def scroll_down(driver, whileSeconds, by):
+        start = datetime.datetime.now()
+        end = start + datetime.timedelta(seconds=whileSeconds)
+        while True:
+            #driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            driver.execute_script('window.scrollBy(0, ' + str(by) + ');')
+            time.sleep(1)
+            if datetime.datetime.now() > end:
+                break
 
 
 @shared_task
@@ -64,6 +77,8 @@ def crawling_start():
         # 무신사 url이면
         if 'musinsa' in crawling_url:
             crawling_musinsa(driver, crawling_url, regexp, f, wr, url_id)
+        elif 'coupang' in crawling_url:
+            crawling_coupang(driver, crawling_url, regexp, f, wr, url_id)
 
     # csv 파일 닫기
     f.close()
@@ -80,7 +95,7 @@ def crawling_start():
 
 
 def crawling_musinsa(driver, crawling_url, regexp, f, wr, url_id):
-    """ PendingUrl을 주소로 젒속해서 크롤링 후 ReviewFile 데이터 생성 """
+    """ crawling_url을 주소로 무신사에 젒속해서 크롤링 후 ReviewFile 데이터 생성 """
     
     # 화면을 읽은 뒤 bs4가 파싱
     html = driver.page_source
@@ -140,6 +155,88 @@ def crawling_musinsa(driver, crawling_url, regexp, f, wr, url_id):
             print("리뷰 마지막 페이지 도착")
             break
 
+
+    # ReviewInfo 데이터 생성
+    review_info = ReviewInfo(url=crawling_url) # url 설정
+    review_info.name = product_name # name 설정
+    # file 설정
+    # 여기에서 에러 발생. File class는 codec cp949??? 확인해보니 UTF-8
+    review_info.file.save(url_id + '.csv', File(f), save=False)
+    
+    # thumbnail 설정
+    review_info.thumbnail.save(url_id + '.jpg', ContentFile(requests.get(image_link).content), save=False)
+
+    # ReviewInfo 데이터 저장
+    review_info.save()
+
+    # make_reveiwzip 함수 큐에 넣기
+    make_reviewzip.delay()
+
+
+
+def crawling_coupang(driver, crawling_url, regexp, f, wr, url_id):
+    """ crawling_url을 주소로 쿠팡에 젒속해서 크롤링 후 ReviewFile 데이터 생성 """
+
+    # 화면을 읽은 뒤 bs4가 파싱
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'lxml')
+
+    # 제품 이미지 get
+    image = soup.select('.prod-image__detail')[0]
+    image_link = "https:" + image["src"]
+
+    # 제품 이름 get
+    product_name = soup.select('.prod-buy-header__title')[0].text.strip()
+    print(product_name)
+
+    # 쿠팡은 초기 로딩에도 시간이 걸리고 아래로 로딩하기 전까지는 데이터가 로딩되지 않는다
+    scroll_down(driver, 5, by=1000)
+
+    # 페이지 별로 크롤링
+    page = 1
+    # 마지막 페이지 도달할 때까지 반복
+    while True:
+        scroll_down(driver, 2, by=500)
+        driver.implicitly_wait(1)
+
+        # 화면을 읽은 뒤 bs4가 파싱
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'lxml')
+
+        # 한 페이지의 리뷰들을 찾아 리스트로 반환
+        # 일반 후기 뿐만이 아니라 다른 후기들도 읽어들이게 되는 문제 발생 -> 클래스를 특정해서 해결
+        reviews = soup.select('.js_reviewArticleContent')
+
+        # 리뷰의 별점들 리스트
+        score_elements = soup.select('.js_reviewArticleRatingValue')
+        scores = [int(score["data-rating"]) for score in score_elements]
+
+        # csv 파일로 저장하는 게 깔끔
+        # .text는 태그 안에 있는 내용만 추출
+        for (idx, review) in enumerate(reviews):
+            wr.writerow([ review.text.strip() ] + [ scores[idx] ])
+
+        # 몇 페이지를 크롤링 중인 지 출력
+        print("page:", page)
+        print(len(reviews))
+        if(len(reviews) == 0):
+            print('별점만 달려 있는 리뷰들만 남았습니다.')
+            break
+
+        # 리뷰 다음 페이지로 이동
+        # 마지막 페이지에 도착하면 반복문 종료
+        page += 1
+        try:
+            next = driver.find_element_by_css_selector('.js_reviewArticlePagingContainer')
+            # 단순 @class는 클래스 전체가 일치하는 지 확인하고 //는 루트 요소부터 전부 탐색한다 
+            # .//는 현재 노드의 하위 노드 탐색
+            if(page % 10 == 1):
+                next.find_element_by_xpath('.//button[contains(@class, "js_reviewArticlePageNextBtn")]').click()
+            else:
+                next.find_element_by_xpath(".//button[text()=" + str(page) + " and not(text()[2])]").click()
+        except Exception as e:
+            print('리뷰 마지막에 도착')
+            break
 
     # ReviewInfo 데이터 생성
     review_info = ReviewInfo(url=crawling_url) # url 설정
